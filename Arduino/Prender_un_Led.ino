d2527ab1d25b4a3f06f1e86319844c5b4d014fa7
Diego Ulises <51557342+IDiegoUlises@users.noreply.github.com>
2020-05-01 17:42:45 -0400

    Update Prender_un_Led.ino

--- Arduino/Prender_un_Led.ino
+++ Arduino/Prender_un_Led.ino
@@ -1,1 +1,18 @@
-f
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+//por defecto usa la direccion 0x40, esto para conectar mas modulos al mismo tiempo
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+void setup()
+{
+  pwm.begin();
+  pwm.setPWMFreq(60);  //~60 Hz
+}
+
+
+void loop()
+{
+  int pin = 0;
+  pwm.setPWM(pin, 4096, 0); //4049 es para entregar 5v 
+}
